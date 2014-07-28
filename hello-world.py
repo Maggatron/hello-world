0c99d4d700e622237454ab4295ab0580b16ee9cb
Maggatron <m.karanasiou@gmail.com>
2014-07-28 17:50:45 +0100

    create hello-world.py
    Plus additional conversation and goodbye points

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+# This program says hello and asks for my name.
+print('Hello world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+
+
+# This program has a little conversation with me.
+print('How are you today?')
+state = input()
+print('I see, you are ' + state)
+print('Why are you ' + state + '?')
+explanation = input()
+
+# This program says goodbye
+print('Blah, blah.. You talk too much, human.')
+print('I will leave you now. I need to go wash my hair.')
+print('Goodbye ' + myName + ' !')
